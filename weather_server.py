c337f6a74936f8bf562708a66331af5b6ef89866
Saifadam Pathan <saifadam.pathan1@ibm.com>
2022-06-18 17:51:11 -0400

    Added a utils file for business logic

--- weather_server.py
+++ weather_server.py
@@ -2,29 +2,33 @@ import grpc
 from concurrent import futures
 import weather_pb2_grpc as pb2_grpc
 import weather_pb2 as pb2
- 
+from weather_utils import get_temperature, get_rel_humidity
+
 class WeatherService(pb2_grpc.WeatherServiceServicer):
  
     def __init__(self, *args, **kwargs):
         pass
 
-
     def GetTemperature(self, request, context):
         city_name = request.name
         print("Temperature request for : {}".format(city_name))
         # get the string from the incoming request
-        return pb2.Temperature(fahrenheit = 77.8, celcius = 21.3)
+        tf, tc = get_temperature(city_name)
+        return pb2.Temperature(fahrenheit = tf, celcius = tc)
 
     def GetHumidity(self, request, context):
         city_name = request.name
         print("Humidity request for : {}".format(city_name))
-        return pb2.RelHumidity(percentage=85.0)
+        relhum = get_rel_humidity(city_name)
+        return pb2.RelHumidity(percentage=relhum)
 
     def GetDetailed(self, request, context):
         city_name = request.name
         print("Humidity request for : {}".format(city_name))
-        t1 = pb2.Temperature(celcius=18.1, fahrenheit=57.3)
-        rh = pb2.RelHumidity(percentage=73)
+        tf, tc = get_temperature(city_name)
+        relhum = get_rel_humidity(city_name)
+        t1 = pb2.Temperature(celcius=tc, fahrenheit=tf)
+        rh = pb2.RelHumidity(percentage=relhum)
         return pb2.Detailed(temperature=t1, rel_humidity=rh)
 
  
